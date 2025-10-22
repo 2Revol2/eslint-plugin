23f82644ae01c0b8620ace3efa983a8940309167
2Revol2 <maksimbaluta02@gmail.com>
2025-10-22 10:38:47 +0200

    feat: add public api imports rule

---
+++ tests/lib/rules/public-api-imports.js
@@ -0,0 +1,57 @@
+/**
+ * @fileoverview imports only from public api
+ * @author maksym
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/public-api-imports"),
+  RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+const aliasOptions = [
+  {
+    alias: '@'
+  }
+]
+
+const ruleTester = new RuleTester();
+ruleTester.run("public-api-imports", rule, {
+  valid: [
+    {
+      code: "import { addCommentFormActions, addCommentFormReducer } from '../../model/slices/addCommentFormSlice'",
+      errors: [],
+    },
+    {
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article'",
+      errors: [],
+      options: aliasOptions,
+    }
+  ],
+
+  invalid: [
+    {
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/model/file.ts'",
+      errors: [{ message: "absolute path should be imported from public api"}],
+      options: aliasOptions,
+    },
+    {
+      filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\entities\\StoreDecorator.tsx',
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing/file.tsx'",
+      errors: [{message: 'absolute path should be imported from public api'}],
+      options: aliasOptions,
+    },
+    {
+      filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\entities\\forbidden.ts',
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing'",
+      errors: [{message: 'absolute path should be imported from public api'}],
+      options: aliasOptions,
+    }
+  ],
+});
